08d22fa6fb819534d6b6511d4c0699ecbdab5e6a
baiyutang <irichardwang@gmail.com>
2020-10-08 21:54:35 +0800

    update

--- code/index.php
+++ code/index.php
@@ -7,15 +7,21 @@ $key = 'first';
 $redis->incr($key);
 
 echo '页面浏览了：'.$redis->get($key);
-die;
 
-$dsn = 'mysql:host=db;port=3306';
-$user = 'root';
-$password = 'root';
 
 try {
-    $dbh = new PDO($dsn, $user, $password);
-    print_r($dbh);
+    $dsn = 'mysql:host=db;port=3306';
+    $user = 'root';
+    $password = 'root';
+
+    $conn = new PDO($dsn, $user, $password);
+    $sql = 'show databases;';
+    $databaseArr = $conn->query($sql)->fetchAll();
+
+    echo "<br>all the databases: ";
+    foreach($databaseArr as $item){
+        echo $item['Database'].',';
+    }
 } catch (PDOException $e) {
     echo 'Connection failed: '.$e->getMessage();
 }
